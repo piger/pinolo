65ad8de86413a7e4b7a2e9c6f289d0c74dbbe1a6
sand <daniel@spatof.org>
2009-09-25 17:47:57 +0200

    con i commenti

--- clients2.py
+++ clients2.py
@@ -32,25 +32,43 @@ class ClienteFactory(ClientFactory):
 	print "started to connect"
 
     def clientConnectionLost(self, connector, reason):
-	self.padre.spegnimi(self)
+	"""Se e' l'ultima connessione di questa Factory, eliminala
+	dal connection manager."""
+	if len(self.clienti) == 0:
+	    self.padre.spegnimi(self)
 
 
 from twisted.internet import reactor
 
 class ConnManager():
+    """Questa classe e' un Connection Manager, si occupa
+    di tenere traccia delle varie Factory e quindi delle
+    loro connessioni.
+    """
+
     def __init__(self):
+	"""Inizializza lo "storage" delle Factory"""
 	self.figli = []
 
     def aggiungi(self, f):
+	"""Aggiunge una factory alla lista, e imposta l'attributo
+	"padre" a se stesso; in questo modo ogni factory puo'
+	chiamare il connection manager."""
 	self.figli.append(f)
 	f.padre = self
 
     def spegni_tutto(self):
+	"""Per ogni figlio (Factory) e per ogni sua connessione
+	(Protocol) chiama il metodo spegni().
+	E' lo shutdown globale chiamato da un protocol."""
 	for f in self.figli:
 	    for c in f.clienti:
 		c.spegni()
 
     def spegnimi(self, figlio):
+	"""Questo viene chiamato da una Factory quando tutte le sue
+	connessioni sono terminate. Elimina la Factory dalla lista,
+	e se questa e' l'ultima, stoppa il reactor."""
 	self.figli.remove(figlio)
 	if len(self.figli) == 0:
 	    reactor.stop()
