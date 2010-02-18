7f12779912e151cdf9e48eedc782f5aac1fcc105
sand <daniel@spatof.org>
2010-02-18 13:25:04 +0100

    Revert "vim :retab"
    This reverts commit b506a7cc1b4d960f0982283d3fd6695d0ca8bdd2.

--- pinolo.py
+++ pinolo.py
@@ -21,11 +21,11 @@ from markov import Markov
 
 __version__ = u'0.2'
 __author__  = u'sand <daniel@spatof.org>'
-#__all__            = [u'Pinolo']
+#__all__	    = [u'Pinolo']
 # http://effbot.org/pyref/__all__.htm
 
-CHARSET     = u'utf-8'
-CONFIG      = 'pinolo.cfg'
+CHARSET	    = u'utf-8'
+CONFIG	    = 'pinolo.cfg'
 QUOTESDB    = 'quotes.db'
 BRAINFILE   = 'brain.b'
 
@@ -36,35 +36,35 @@ class ConnManager():
     """
 
     def __init__(self):
-        """Inizializza lo "storage" delle Factory"""
-        self.figli = []
-        self.dbh = db.DbHelper("quotes.db")
-        self.brain = Markov(brain_file=BRAINFILE)
+	"""Inizializza lo "storage" delle Factory"""
+	self.figli = []
+	self.dbh = db.DbHelper("quotes.db")
+	self.brain = Markov(brain_file=BRAINFILE)
 
     def aggiungi(self, f):
-        """Aggiunge una factory alla lista, e imposta l'attributo
-        "padre" a se stesso; in questo modo ogni factory puo'
-        chiamare il connection manager."""
-        self.figli.append(f)
-        f.padre = self
+	"""Aggiunge una factory alla lista, e imposta l'attributo
+	"padre" a se stesso; in questo modo ogni factory puo'
+	chiamare il connection manager."""
+	self.figli.append(f)
+	f.padre = self
 
     def spegni_tutto(self):
-        """Per ogni figlio (Factory) e per ogni sua connessione
-        (Protocol) chiama il metodo spegni().
-        E' lo shutdown globale chiamato da un protocol."""
-        for f in self.figli:
-            f.quitting = True
-            for c in f.clienti:
-                c.quit("ME LO HAN DETTO!")
+	"""Per ogni figlio (Factory) e per ogni sua connessione
+	(Protocol) chiama il metodo spegni().
+	E' lo shutdown globale chiamato da un protocol."""
+	for f in self.figli:
+	    f.quitting = True
+	    for c in f.clienti:
+		c.quit("ME LO HAN DETTO!")
 
     def spegnimi(self, figlio):
-        """Questo viene chiamato da una Factory quando tutte le sue
-        connessioni sono terminate. Elimina la Factory dalla lista,
-        e se questa e' l'ultima, stoppa il reactor."""
-        self.figli.remove(figlio)
-        if len(self.figli) == 0:
-            self.brain.dump_brain()
-            reactor.stop()
+	"""Questo viene chiamato da una Factory quando tutte le sue
+	connessioni sono terminate. Elimina la Factory dalla lista,
+	e se questa e' l'ultima, stoppa il reactor."""
+	self.figli.remove(figlio)
+	if len(self.figli) == 0:
+	    self.brain.dump_brain()
+	    reactor.stop()
 
 def main():
     import ConfigParser
@@ -80,22 +80,22 @@ def main():
     servers = []
 
     for section in config.sections():
-        if section.startswith("Server"):
-            server = {
-                    'name': config.get(section, 'name'),
-                    'address':  config.get(section, 'server'),
-                    'port':     int(config.get(section, 'port')),
-                    'nickname': config.get(section, 'nickname'),
-                    'channels': config.get(section, 'channels').split(", ")
-            }
-            servers.append(server)
+	if section.startswith("Server"):
+	    server = {
+		    'name': config.get(section, 'name'),
+		    'address':	config.get(section, 'server'),
+		    'port':	int(config.get(section, 'port')),
+		    'nickname':	config.get(section, 'nickname'),
+		    'channels':	config.get(section, 'channels').split(", ")
+	    }
+	    servers.append(server)
 
     c = ConnManager()
 
     for server in servers:
-        f = PinoloFactory(server)
-        c.aggiungi(f)
-        reactor.connectTCP(server['address'], server['port'], f)
+	f = PinoloFactory(server)
+	c.aggiungi(f)
+	reactor.connectTCP(server['address'], server['port'], f)
 
     reactor.run()
 
