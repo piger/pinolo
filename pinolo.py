e5b762910ba81d209c1216646ed5a046cf2f0a80
sand <daniel@spatof.org>
2010-02-19 15:09:33 +0100

    bump version

--- pinolo.py
+++ pinolo.py
@@ -22,7 +22,7 @@ from irc import *
 import db
 #import mh_python
 
-__version__ = '0.2.1a'
+__version__ = '0.2.2b'
 __author__  = 'sand <daniel@spatof.org>'
 #__all__            = ['Pinolo']
 # http://effbot.org/pyref/__all__.htm
