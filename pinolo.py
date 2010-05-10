91d5e3c74d666d28dd7affef41b16e4ca16411c6
sand <daniel@spatof.org>
2010-05-10 14:17:13 +0200

    BSD license

--- pinolo.py
+++ pinolo.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
-
-"""Pinolo"""
+# Copyright (c) 2010, sand <daniel@spatof.org>
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation and/or
+#    other materials provided with the distribution.
+# 3. The name of the author nor the names of its contributors may be used to
+#    endorse or promote products derived from this software without specific prior
+#    written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 import sys
-
 # verify python version is high enough
 if sys.version_info[0] * 10 + sys.version_info[1] < 25:
     error = RuntimeError('pinolo requires python 2.5 or higher')
@@ -24,8 +45,6 @@ import db
 
 __version__ = '0.2.4b'
 __author__  = 'sand <daniel@spatof.org>'
-#__all__            = ['Pinolo']
-# http://effbot.org/pyref/__all__.htm
 
 CHARSET     = 'utf-8'
 DEFAULT_CONFIG_FILE = 'pinolo.cfg'
