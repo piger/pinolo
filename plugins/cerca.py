213a1ffed61ffab02851a532a1e33e9c9b1f6b20
sand <daniel@spatof.org>
2011-08-24 14:18:15 +0200

    correzione plugin cerca

--- plugins/cerca.py
+++ plugins/cerca.py
@@ -6,9 +6,11 @@ Google Search plugin
 http://code.google.com/apis/websearch/docs/reference.html#_intro_fonje
 """
 
+import re
 import urllib
 import simplejson
 from BeautifulSoup import BeautifulSoup
+import htmlentitydefs
 
 from pinolo.main import CommandPlugin, PluginActivationError
 from pinolo import MyOptionParser, OptionParserError
@@ -37,6 +39,59 @@ def strip_html(blob):
     s = BeautifulSoup(blob, convertEntities=BeautifulSoup.HTML_ENTITIES)
     page = u''.join(s.findAll(text=True))
     return page
+
+# from: http://effbot.org/zone/re-sub.htm#unescape-html
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+
+        return text
+    return re.sub("&#?\w+;", fixup, text)
+
+def new_strip_html(text):
+    def fixup(m):
+        text = m.group(0)
+
+        if text[:1] == "<":
+            return "" # ignore tags
+
+        if text[:2] == "&#":
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+
+        elif text[:1] == "&":
+            entity = htmlentitydefs.entitydefs.get(text[1:-1])
+            if entity:
+                if entity[:2] == "&#":
+                    try:
+                        return unichr(int(entity[2:-1]))
+                    except ValueError:
+                        pass
+                else:
+                    return unicode(entity, 'iso-8859-1')
+        return text
+    return re.sub("(?s)<[^>]*>|&#?\w+;", fixup, text)
+
 
 def query_google(search):
     query = urllib.urlencode({
