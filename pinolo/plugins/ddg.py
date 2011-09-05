dc87252d80ab3ac0ca1a3bbfacd85890bf647f7b
sand <daniel@spatof.org>
2011-09-05 10:05:15 +0200

    update ddg

--- pinolo/plugins/ddg.py
+++ pinolo/plugins/ddg.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
 import urllib
 import json
+import itertools
 from pinolo.plugins import Plugin
 from pinolo.utils import gevent_url_open
 
 SEARCH_URL = "http://api.duckduckgo.com/"
 MAX_RESULTS = 5
 SEARCH_LANG = "it"
+TYPES = {
+    u'A': u'article',
+    u'D': u'disambiguation',
+    u'C': u'category',
+    u'N': u'name',
+    u'E': u'exclusive',
+    u'': u'nothing',
+}
+
 
 def search_ddg(text):
     query = urllib.urlencode({
@@ -27,16 +38,30 @@ def search_ddg(text):
         data = response.read()
         data.decode('utf-8', 'replace')
     r = json.loads(data)
+    # toglie i value 'empty'
+    # pr = dict((k, v) for k,v in r.items() if v)
 
-    print r
+    # tolgo i value vuoti o nulli e i value duplicati.
+    # mi serve perche' il dict con i risultati contiene per alcuni campi sia la versione
+    # plaintext che quella HTML, e io uso `no_html`.
+    results = {}
+    for k, v in r.items():
+        if type(v) is list:
+            if len(v) > 0:
+                results[k] = r.pop(k)
+            else:
+                r.pop(k)
+    rr = dict((v, k) for k, v in r.items() if v)
+    results.update(dict((k, v) for v, k in rr.items()))
 
-    if 'Results' in r:
-        results = r['Results']
-    else:
-        results = []
-    t = r.get('Type', u'')
+    if 'Results' in results:
+        results['Results'] = results['Results'][:5]
 
-    return { 'type': t, 'results': results }
+    if 'Type' in results:
+        t = results['Type']
+        results['Type'] = TYPES.get(t, u'')
+
+    return results
 
 class DuckDuckGoPlugin(Plugin):
     COMMAND_ALIASES = {
@@ -46,6 +71,8 @@ class DuckDuckGoPlugin(Plugin):
     def on_cmd_duckduckgo(self, event):
         if not event.text: return
         results = search_ddg(event.text)
-        event.reply(u"type: %s" % results['type'])
-        for result in results['results']:
-            event.reply(u"%s %s" % (result['FirstURL'], result['Text']))
+        if not len(results.keys()):
+            event.reply(u"DDG busta de piscio")
+        else:
+            for k, v in results.items():
+                event.reply(u"%s: %s" % (k, v))
