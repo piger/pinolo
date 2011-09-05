6c866c6b79157765dfd68a95500bd0faa71234b1
sand <daniel@spatof.org>
2011-09-05 10:21:41 +0200

    update utils

--- pinolo/utils.py
+++ pinolo/utils.py
@@ -3,6 +3,11 @@ import hashlib
 import httplib
 import urllib2
 import htmlentitydefs
+import subprocess
+import errno
+import sys
+import os
+import fcntl
 
 import gevent
 from gevent import socket
@@ -55,45 +60,45 @@ def md5(text):
 
 class gevent_HTTPConnection(httplib.HTTPConnection):
     """
-    httplib/urllib2 + gevent
-    modificata a mano perche' la versione in rete SUCA
+    httplib.HTTPConnection compatibile con gevent.
+    - da python 2.6
 
     Per evitare monkey.patch_all():
     http://groups.google.com/group/gevent/browse_thread/thread/c20181cb066ee97e?fwc=2&pli=1
     """
     def connect(self):
         self.sock = socket.create_connection((self.host, self.port),
-                                             self.timeout, self.source_address)
+                                             self.timeout)
         if self._tunnel_host:
             self._tunnel()
 
 class gevent_HTTPHandler(urllib2.HTTPHandler):
+    """
+    urllib2.HTTPHandler compatibile con gevent.
+    """
     def http_open(self, request):
         return self.do_open(gevent_HTTPConnection, request)
 
-def gevent_url_open(url, headers=None, data=None):
+def gevent_url_open(url, headers=[], data=None):
+    """
+    Fa una richiesta HTTP GET o POST con eventuali `headers` aggiunti.
+    E' compatibile con gevent.
+    """
     request = urllib2.Request(url, data)
     request.add_header('User-Agent', USER_AGENT)
 
-    if headers:
-        for name, value in headers:
-            request.add_header(name, value)
+    for name, value in headers:
+        request.add_header(name, value)
 
     opener = urllib2.build_opener(gevent_HTTPHandler)
-    response = opener.open(request)
-    return response
-
-# from gevent/examples
-
-import subprocess
-import errno
-import sys
-import os
-import fcntl
+    return opener.open(request)
 
 
 def popen_communicate(args, data=''):
-    """Communicate with the process non-blockingly."""
+    """
+    Communicate with the process non-blockingly.
+    # from gevent/examples
+    """
     p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     fcntl.fcntl(p.stdin, fcntl.F_SETFL, os.O_NONBLOCK)  # make the file nonblocking
     fcntl.fcntl(p.stdout, fcntl.F_SETFL, os.O_NONBLOCK)  # make the file nonblocking
